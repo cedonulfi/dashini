3f2f1a11406d6567eb06fdad21101074cab51b29
Eko Sulistiyono <55071271+cedonulfi@users.noreply.github.com>
2024-12-11 21:54:17 +0700

    Create login.php
    login form

---
+++ dashboard/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="checklogin.php" method="post">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
